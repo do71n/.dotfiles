f62cdc11d4d9271bbd71b98cdd49f3db33a23371
Howard <62504060+Howard-K@users.noreply.github.com>
2026-07-10 18:41:56 +0800

    feat(nvim): Automatic center after cursor moving to forward position

--- nvim/lua/howard/remap.lua
+++ nvim/lua/howard/remap.lua
@@ -10,6 +10,7 @@ vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-o>", "<C-o>zz")
+vim.keymap.set("n", "<C-i>", "<C-i>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
