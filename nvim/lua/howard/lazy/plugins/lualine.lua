c52e59edb42b4685152cc15d64b3454ad26d4989
Howard <62504060+Howard-K@users.noreply.github.com>
2026-07-27 02:43:24 +0800

    feat(nvim): create lualine.lua for nvim line aesthetic

---
+++ nvim/lua/howard/lazy/plugins/lualine.lua
@@ -0,0 +1,73 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' }, -- Optional, for file icons
+  config = function()
+    local function buffer_number()
+      return string.format('[b%d]', vim.api.nvim_get_current_buf())
+    end
+
+    local function line_total()
+      return string.format('%d/%d', vim.fn.line('.'), vim.fn.line('$'))
+    end
+
+    local function column_number()
+      return string.format('[col %d]', vim.fn.col('.'))
+    end
+
+    local function hex_under_cursor()
+      local char = vim.fn.matchstr(vim.fn.getline('.'), '\\%' .. vim.fn.col('.') .. 'c.')
+      if char == '' then
+        return 'Hex:0x00'
+      end
+      return string.format('Hex:0x%02X', vim.fn.char2nr(char))
+    end
+
+    require('lualine').setup({
+      options = {
+        theme = 'auto',
+        icons_enabled = true,
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+        disabled_filetypes = {
+          statusline = { 'lazygit' },
+        },
+      },
+      sections = {
+        lualine_a = {
+          { buffer_number, color = { gui = 'bold' } },
+        },
+        lualine_b = {
+          { 'branch', icon = '', color = { gui = 'bold' } },
+          { 'diff', symbols = { added = '+', modified = '~', removed = '-' }, colored = true },
+          'fileformat',
+          { 'filetype', icon_only = true },
+        },
+        lualine_c = {
+          {
+            'filename',
+            path = 1,
+            symbols = { modified = ' [+]', readonly = ' [r]', unnamed = '[No Name]' },
+          },
+        },
+        lualine_x = {
+          'encoding',
+        },
+        lualine_y = {
+          line_total,
+          column_number,
+        },
+        lualine_z = {
+          hex_under_cursor,
+        },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { { 'filename', path = 1 } },
+        lualine_x = { line_total },
+        lualine_y = {},
+        lualine_z = {},
+      },
+    })
+  end,
+}
