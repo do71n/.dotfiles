5bdcdcc95a230996db4d9d175a57435de83e4bed
howardk <62504060+do71n@users.noreply.github.com>
2026-07-29 02:36:12 +0800

    feat(nvim): completion feature upgrade
    - limit to 20 for lsp suggestion and 10 cmd suggestion for reducing
    visual noise
    - added border to documentation and completion
    - added lspkind for vscode like styple formatting

--- nvim/lua/howard/lazy/plugins/syntax/completion.lua
+++ nvim/lua/howard/lazy/plugins/syntax/completion.lua
@@ -1,42 +1,57 @@
 return { "hrsh7th/nvim-cmp",
-
+    event = { "InsertEnter", "CmdlineEnter" },
     dependencies = {
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-cmdline",
-        "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
+        "L3MON4D3/LuaSnip",
+        "onsails/lspkind.nvim",
         "nvim-tree/nvim-web-devicons",
     },
 
     config = function ()
+        -- vim.o.winborder = "rounded"
+
         local cmp = require('cmp')
+        local lspkind = require('lspkind')
 
         -- autocompletion with lausnip
         local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
         cmp.setup({
+            completion = {
+                completeopt = "menu,menuone,noinsert", -- allow auto-select
+            },
             snippet = {
                 expand = function(args)
                     require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
                 end,
             },
             window = {
-                completion = cmp.config.window.bordered(),
-                documentation = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered({ border = "rounded" }),
+                completion = cmp.config.window.bordered({ border = "rounded", max_height = 20 }), -- Keeps the popup box from growing taller than 10 lines
             },
             mapping = cmp.mapping.preset.insert({
                 ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                 ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                ["<C-f>"] = cmp.mapping.scroll_docs(5),
+                ["<C-b>"] = cmp.mapping.scroll_docs(-5),
                 ['<C-y>'] = cmp.mapping.confirm({ select = true }),
                 ["<C-space>"] = cmp.mapping.complete(),
             }),
-
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
                 { name = 'luasnip' }, -- For luasnip users.
-                { name = 'buffer' }
-            })
+                { name = 'buffer' }, -- word suggestion from current buffer
+                { name = 'path' }
+            }),
+            formatting = {
+                format = lspkind.cmp_format({
+                    maxwidth = 50,          -- 1. Truncates long completion items
+                    ellipsis_char = "...",  -- 2. Appends '...' when truncated
+                }),
+            },
         })
 
     end
