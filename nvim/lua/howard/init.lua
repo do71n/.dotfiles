feb5fdf602ce02f0974a50a33776711d7b64bddf
howardk <62504060+do71n@users.noreply.github.com>
2026-07-29 02:18:56 +0800

    refactor(nvim): refactored LSP keybind into lsp.lua

--- nvim/lua/howard/init.lua
+++ nvim/lua/howard/init.lua
@@ -2,8 +2,8 @@ require("howard.remap")
 require("howard.set")
 require("howard.lazy.lazy_init")
 
-local autocmd = vim.api.nvim_create_autocmd
 -- Seperating 2 Groups is not necessarily needed, but we do it this way.
+local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 local TheHowardGroup = augroup('Howard', {})
 local yank_group = augroup('HighlightYank', {})
@@ -30,28 +30,9 @@ autocmd('TextYankPost', {
     end,
 })
 
--- Strip trailing whitespace on save, read the command
+-- Strip trailing whitespace on save by reading the command (good workaround)
 autocmd({"BufWritePre"}, {
     group = TheHowardGroup,
     pattern = "*",
     command = [[%s/\s\+$//e]],
-})
-
--- Set LSP Keymaps once an LSP server attaches to the buffer
-autocmd('LspAttach', {
-    group = TheHowardGroup,
-    callback = function(e)
-        local opts = { buffer = e.buf }
-        -- Press shift + K to open documentation
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        -- vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end,opts)
-        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.jump({count = 1}) end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.jump({count = -1}) end, opts)
-    end
 })
